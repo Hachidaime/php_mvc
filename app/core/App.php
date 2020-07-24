505c196ed3c00dd69aa375e9dda95072e487fb39
Hachidaime <cold.kid40@gmail.com>
2020-07-24 22:40:43 +0700

    Routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,19 @@ class App
 {
   public function __construct()
   {
-    echo "OK";
+    $url = rtrim($this->parseUrl(), '/');
+    $url = filter_var($url, FILTER_SANITIZE_URL);
+    $url = explode('/', $url);
+    var_dump($url);
+  }
+
+  public function parseUrl()
+  {
+    $issetUrl = isset($_GET['url']);
+
+    if ($issetUrl) {
+      $url = $_GET['url'];
+      return $url;
+    }
   }
 }
