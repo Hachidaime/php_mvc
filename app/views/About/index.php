cf8c152717226a5d84616a3ec7dc22d208066cdb
Hachidaime <cold.kid40@gmail.com>
2020-07-25 02:12:34 +0700

    Asset

--- app/views/About/index.php
+++ app/views/About/index.php
@@ -1,2 +1,3 @@
-<h1>About Me</h1>
+<h1 class="mt-4">About Me</h1>
+<img src="<?= BASE_URL; ?>/img/img_avatar2.png" class="rounded-circle shadow" width="200" alt="">
 <p>Halo nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya seorang <?= $data['pekerjaan']; ?>,</p>
